b310a8c0bb2ff4c2f3d6f3d99a9483b351c5e378
Julio Delgado <juliom6@gmail.com>
2020-05-06 22:06:59 -0300

    avancando a criacao do modelo no banco de dados

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,46 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+
+##### Configurar o banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bancoDeDados.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+#####
+
+##### Criar o modelo para o registro do usuario
+# class Usuario(db.Model):
+#     codigo = 
+
+class Apontamento(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome_usuario = db.Column(db.String(25), nullable=True)
+    hora = db.Column(db.DateTime, default=datetime.now)
+
+db.create_all()
 
 @app.route('/inicio')
 @app.route('/')
-def inicio():
+def mostra_inicio():
     return render_template('index.html')
 
-@app.route('/inicio2')
-def outro_inicio():
-    return render_template('alternative_index.html')
+# @app.route('/api/funcionarios', methods=['GET', 'POST'])
+# def funcionarios():
+#     usuario = Apontamento(nome_usuario=)
+#     db.session.add(usuario)
+#     db.session.commit()
 
 
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
 
 ## Para rodar o projeto em desenvolvimento
 
